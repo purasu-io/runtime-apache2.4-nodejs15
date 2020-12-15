5427781b129482ac30885e555beee6f8e7004945
Petrus Repo <petrus.repo@iki.fi>
2020-12-15 11:45:05 +0200

    Create CPU burner example for load testing

--- volumes/a-shared/app/index.js
+++ volumes/a-shared/app/index.js
@@ -3,8 +3,24 @@ const http = require('http');
 const port = 8081;
 
 http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello from Node.js.\n');
+   response.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+      'Transfer-Encoding': 'chunked',
+   });
+
+   if (request.url == "/") {
+      response.end('Hello from Node.js - <a href="/cpu">heat the cpu</a>\n');
+   } else if (request.url == "/cpu") {
+      response.write("Start CPU BURN.<br>\n");
+      for (let i=0; i<1000000000; i++) {
+         if (i % 10000000 == 0) {
+            response.write(`Incremental load: ${i}<br>\n`);
+         }
+      }
+      response.end('Finish CPU BURN - back to <a href="/">index</a>.<br>\n');
+   } else {
+      response.end("Node.js - Unknown route.")
+   }
 }).listen(port);
 
 console.log(`Server running at http://127.0.0.1:${port}/`);
